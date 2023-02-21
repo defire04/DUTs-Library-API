403c7e62d71149e945e32f9ff6ab806e65870a6f
Dima Olaba <defire04st@gmail.com>
2023-02-21 15:31:23 +0200

    Initial commit

---
+++ src/main/java/com/example/dutslibraryapi/DuTsLibraryApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.dutslibraryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuTsLibraryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuTsLibraryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dutslibraryapi/DuTsLibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dutslibraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuTsLibraryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
